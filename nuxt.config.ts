3b99d14feb33f080c57d01c059fcdce88293d24d
Luke_YS <yjsmk0902@gmail.com>
2025-04-02 19:42:35 +0900

    init: fix config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,12 @@ import {process} from "std-env";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  app: {
+    baseURL: "/main"
+  },
   runtimeConfig: {
     public: {
       apiBaseUrl: 'http://localhost:8080',
-      baseURL: '/main',
     }
   }
 })
