679f8672f98fdf81d8b99aa839723c8676f20061
Luke_YS <yjsmk0902@gmail.com>
2025-04-02 20:48:09 +0900

    init: implement API Proxy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,21 @@
 import {process} from "std-env";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  app: {
-    baseURL: "/main"
-  },
-  runtimeConfig: {
-    public: {
-      apiBaseUrl: 'http://kube-test-server-main.default.svc.cluster.local/main-api',
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    app: {
+        baseURL: "/main"
+    },
+    nitro: {
+      routeRules: {
+        '/api/**': {
+          proxy: 'http://ing-default-kubeprivatei-95da4-104258556-e369bd066aff.kr.lb.naverncp.com/main-api/**'
+        }
+      }
+    },
+    runtimeConfig: {
+        public: {
+            apiBaseUrl: '/api',
+        }
     }
-  }
 })
